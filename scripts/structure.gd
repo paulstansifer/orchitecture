0ef06763e26bbf37eccce5c6e77c6be7845be66d
Paul Stansifer <paul.stansifer@gmail.com>
2025-04-21 16:32:56 -0400

    Clean up representation of buildable things

--- scripts/structure.gd
+++ scripts/structure.gd
@@ -2,7 +2,8 @@ extends Resource
 class_name Structure
 
 @export_subgroup("Model")
-@export var model:PackedScene # Model of the structure
+@export var model: PackedScene # Model of the structure
+@export var id: int # index into the MeshLibrary. Set at load time.
 
 @export_subgroup("Gameplay")
-@export var price:int # Price of the structure when building
+@export var price: int # Price of the structure when building
