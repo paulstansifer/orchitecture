efe548ab513e088f11dad3ed270ac203a46c52ea
Paul Stansifer <paul.stansifer@gmail.com>
2026-05-20 15:25:30 -0400

    Add three additional suns. Unrealistic, but makes all the windows show light.

--- src/world.rs
+++ src/world.rs
@@ -1,19 +1,29 @@
+use std::f32::consts::{FRAC_PI_2, FRAC_PI_4, PI};
+
 use bevy::prelude::*;
 
-/// Startup system: spawns the directional light and ground plane.
+/// Startup system: spawns four directional lights and the ground plane.
 pub fn spawn_world(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut ambient_light: ResMut<GlobalAmbientLight>,
 ) {
-    commands.spawn((
-        DirectionalLight {
-            illuminance: 10_000.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_rotation(Quat::from_euler(EulerRot::XYZ, -0.8, 0.4, 0.0)),
-    ));
+    // Four lights may be costly; reconsider if performance is bad.
+    for azimuth in [0.0, FRAC_PI_2, PI, -FRAC_PI_2] {
+        commands.spawn((
+            DirectionalLight {
+                illuminance: 2_500.0,
+                // We should try `TemporalAntiAliasing` to reduce the noise.
+                shadows_enabled: true,
+                soft_shadow_size: Some(10.0),
+                ..default()
+            },
+            Transform::from_rotation(Quat::from_euler(EulerRot::YXZ, azimuth, -FRAC_PI_4, 0.0)),
+        ));
+    }
+
+    ambient_light.brightness = 100.0;
 
     commands.spawn((
         Mesh3d(meshes.add(Plane3d::default().mesh().size(100.0, 100.0))),
